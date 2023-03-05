7db6f7b8c74dbca436491e85fc7a84af8ef0a30b
mxnyers <mxnyers@iu.edu>
2023-03-04 23:04:48 -0800

    Created Multiple pages with different components
    - installed node route packages
    - created new file structure for multiple pages
    - added navbar functionality
    - pulled api to a specefic page
    - created footer to a page
    - added functionality to add new pages and send them to the correct route
    - created welcome screen

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,46 +1,25 @@
 import React, { Component } from 'react';
+import Footer from './common-componenets/footer';
+import Navbar from './common-componenets/navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Users from './pages/users';
+import './App.css'
+import Home from './pages/home';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state= {
-      items:[],
-      isLoaded: false,
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          items: json, 
-        })
-      });
-  }
 
   render() {  
-    
-    var { isLoaded, items } = this.state;
-
-    if(!isLoaded) {
-      return <div>Loading...</div>;
-    }
-
-    else{
       return (
-        <div className="App">
-          <ul>
-            {items.map(item => (
-              <li key={item.id}>
-                Name: {item.name} | Email: {item.email}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+          
+          <Footer />
+        </Router>
       );
-    }
   }
 }
 
---
+++ react-app/src/pages/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+class Home extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Welcome to the home screen</h1>
+            </div>
+          );
+        };
+    };
+  
+  
+export default Home;
